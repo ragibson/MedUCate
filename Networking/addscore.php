211d0624e1d08f20ec23fe80f4c9aec2b6551161
Dufferl2407 <windwaker225@gmail.com>
2017-04-24 12:25:32 -0400

    Updated PHP scripts

--- Networking/addscore.php
+++ Networking/addscore.php
@@ -5,18 +5,21 @@
     $username = 'USERNAME_GOES_HERE';
     $password = 'PASSWORD_GOES_HERE';
     $database = 'DATABASE_GOES_HERE';
-
     try {
         $dbh = new PDO('mysql:host=' . $hostname . ';dbname=' . $database, $username, $password);
-    } catch(PDOException $e) {
+    }
+
+    catch(PDOException $e) {
         echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() , '</pre>';
     }
 
-    $sth = $dbh->prepare('INSERT INTO scores VALUES (null, :name, :score)');
+    $sth = $dbh->prepare('INSERT INTO scores VALUES (null, :name, :score) ON DUPLICATE KEY UPDATE score = :score');
     try {
         $sth->execute($_GET);
-    } catch(Exception $e) {
-        echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() , '</pre>';
     }
 
+    catch(Exception $e) {
+        echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() , '</pre>';
+    }
+    
 ?>
--- Networking/display.php
+++ Networking/display.php
@@ -7,19 +7,22 @@
     $database = 'DATABASE_GOES_HERE';
 
     try {
-        $dbh = new PDO('mysql:host=' . $hostname . ';dbname=' . $database, $username, $password);
+        $dbh = new PDO('mysql:host='. $hostname .';dbname='. $database, $username, $password);
     } catch(PDOException $e) {
-        echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() , '</pre>';
+        echo '<h1>An error has occurred.</h1><pre>', $e->getMessage() ,'</pre>';
     }
 
     $sth = $dbh->query('SELECT * FROM scores ORDER BY score DESC LIMIT 5');
     $sth->setFetchMode(PDO::FETCH_ASSOC);
+
     $result = $sth->fetchAll();
 
-    if (count($result) > 0) {
+    if(count($result) > 0) {
+        echo "ONE MAN ARMY LEADERBOARD", "\n", "\n";
+        echo "Player  -  Reputation", "\n", "\n";
         foreach($result as $r) {
-            echo $r['name'], "\t", $r['score'], "\n";
+            echo $r['name'], "\t -  ", $r['score'], "\n";
         }
     }
-
+    
 ?>
